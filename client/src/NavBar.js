69e43b58a83b0fbb0060711abbf43df4b9aada8d
TravisSLee <travisslee223@gmail.com>
2021-12-16 15:50:35 -0500

    work on bootstrap

--- client/src/NavBar.js
+++ client/src/NavBar.js
@@ -1,11 +1,17 @@
 import React from 'react'
-import { Link } from "react-router-dom";
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
 
 export default function NavBar() {
     return (
-        <div>
-            <Link to="/">Home</Link>
-            <Link to="/about">About</Link>
-        </div>
+            <Navbar >
+                <Container>
+                    <Nav className="me-auto">
+                        <Nav.Link href="/">Home</Nav.Link>
+                        <Nav.Link href="/about">About</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
     )
 }
--- client/src/Pagination.js
+++ client/src/Pagination.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Button } from 'react-bootstrap';
 
 export default function Pagination({ gotoNextPage, gotoPrevPage }) {
   return (
     <div>
-      {gotoPrevPage && <button onClick={gotoPrevPage}>Previous</button>}
-      {gotoNextPage && <button onClick={gotoNextPage}>Next</button>}
+      {gotoPrevPage && <Button onClick={gotoPrevPage}>Previous</Button>}
+      {gotoNextPage && <Button onClick={gotoNextPage}>Next</Button>}
     </div>
   )
 }
